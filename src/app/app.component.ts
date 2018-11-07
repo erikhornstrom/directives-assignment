7b5af9df592d20a1a0af3c0cd11fa216e23396b0
erikhornstrom <erik.hornstrom@live.se>
2018-11-07 23:53:09 +0100

    finished assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directives-assignment';
+
+  isHidden = false;
+
+  elementsArray = [];
+  needsStyling = false;
+
+  elementName = 'Element';
+
+  onAddButtonClick() {
+    // toggles the 'Shown' paragraph
+    this.isHidden = !this.isHidden;
+    this.elementsArray.push(new Date());
+  }
+
+  getColor() {
+    return this.elementsArray.length >= 5 ? 'blue' : '';
+  }
+
 }
